a409d07705045df5fe8dca67857b7b287bcd56f9
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-02 23:17:32 +0300

    extends the functionality of the testing script

--- py/numpy_test/test.py
+++ py/numpy_test/test.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
 
+import matplotlib.pyplot as plt
 import numpy as np
 
-x1 = np.arange(0, 10.1, 0.1)
-x2 = np.linspace(0, 10, 101)
+fig = 'dockerized_plot.pdf'
 
 
-print(x1)
-print(x2)
+class Functions:
+    def function1(self, arg):
+        return arg * arg + 1
+
+    def function2(self, arg):
+        return 2 * arg * arg + 1
+
+    def function3(self, arg):
+        with np.errstate(divide='ignore'):
+            x = 2 * np.log(arg) + np.sin(arg)
+        return x
+
+
+x1 = np.arange(0, 3, 0.1)
+x2 = np.linspace(0, 10, 21)
+
+plt.plot(x1, list(map(Functions().function1, x1)), '-or', label='f1')
+plt.plot(x1, list(map(Functions().function2, x1)), '-og', label='f2')
+plt.plot(x1, list(map(Functions().function3, x1)), '-ob', label='f3')
+plt.legend(loc='best')
+plt.savefig(fig, bbox_inches='tight', dpi=300)
