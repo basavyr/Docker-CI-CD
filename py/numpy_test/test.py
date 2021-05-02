a297d73d99904829999a3643529b20dc6fe7b983
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-02 23:59:15 +0300

    improves the method for plotting

--- py/numpy_test/test.py
+++ py/numpy_test/test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
+import numpy.random as rd
 import numpy as np
 
 fig = 'dockerized_plot.pdf'
@@ -10,25 +11,32 @@ class Functions:
     def function1(self, arg):
         return arg * arg + 1
 
-    def function2(self, arg):
-        return 2 * arg * arg + 1
+    def function2(self, arg, a):
+        return a * arg * arg + 1
 
-    def function3(self, arg):
+    def function3(self, arg, a):
         with np.errstate(divide='ignore'):
-            x = 2 * np.log(arg) + np.sin(arg)
+            x = a * np.log(arg) + np.sin(arg)
         return x
 
 
 x1 = np.arange(0, 3, 0.1)
 
-PLOT = 1
 
-if(PLOT):
+a1 = rd.choice([1, 2, 4])
+a2 = rd.choice([1, 2, 3])
+
+
+def make_plot(a1, a2):
     print('Starts the plotting procedure')
+    print(f'a1 -> {a1}')
+    print(f'a2 -> {a2}')
 
     plt.plot(x1, list(map(Functions().function1, x1)), '-or', label='f1')
-    plt.plot(x1, list(map(Functions().function2, x1)), '-og', label='f2')
-    plt.plot(x1, list(map(Functions().function3, x1)), '-ob', label='f3')
+    plt.plot(x1, list(map(Functions().function2, x1,
+                          [a1 for x in x1])), '-og', label='f2')
+    plt.plot(x1, list(map(Functions().function3, x1,
+                          [a2 for x in x1])), '-ob', label='f3')
     plt.legend(loc='best')
     plt.savefig(fig, bbox_inches='tight', dpi=300)
 
@@ -36,10 +44,4 @@ if(PLOT):
     print(f'Plot saved at -> {fig}')
 
 
-def t_fct(x1, a):
-    return x1 + a
-
-
-x_in = [1, 2, 3]
-
-print(f'{x_in} -> {list(map(t_fct, x_in,[2 for x in x_in]))}')
+make_plot(a1, a2)
