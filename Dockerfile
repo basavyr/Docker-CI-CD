f039379b4e14e3c4d5a15e44e6ca02d7106fbd5c
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-02 10:13:37 +0300

    add simple numpy test

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+# syntax=docker/dockerfile:1
+FROM basavyr/pyenv-ubuntu:latest
+
+
+SHELL ["usr/bin/bash", "-c"]
+
+COPY . .
+
+CMD ["ls -l"]
---
+++ py/numpy_test/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+x1 = np.arange(0, 10.1, 0.1)
+x2 = np.linspace(0, 10, 101)
+
+
+print(x1)
+print(x2)
