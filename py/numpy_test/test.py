938011e29e77db6868775ebebde377b9e2850ac8
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-03 12:20:29 +0300

    add system information

--- py/numpy_test/test.py
+++ py/numpy_test/test.py
@@ -3,6 +3,9 @@
 import matplotlib.pyplot as plt
 import numpy.random as rd
 import numpy as np
+import time
+import os
+import platform
 from datetime import datetime
 
 now = lambda: str(datetime.utcnow())[
@@ -24,17 +27,10 @@ class Functions:
         return x
 
 
-x1 = np.arange(0, 3, 0.1)
-
-
-a1 = rd.choice([1, 2, 4])
-a2 = rd.choice([1, 2, 3])
-
-
-def make_plot(a1, a2):
+def make_plot(x1, a1, a2):
     print('Starts the plotting procedure')
-    print(f'a1 -> {a1}')
-    print(f'a2 -> {a2}')
+    # print(f'a1 -> {a1}')
+    # print(f'a2 -> {a2}')
 
     plt.plot(x1, list(map(Functions().function1, x1)), '-or', label='f1')
     plt.plot(x1, list(map(Functions().function2, x1,
@@ -46,7 +42,17 @@ def make_plot(a1, a2):
     fig_name = fig + '-' + now() + '.pdf'
     plt.savefig(fig_name, bbox_inches='tight', dpi=300)
     print('Finishing the plotting procedure')
+    time.sleep(1)
+    plt.close()
     print(f'Plot saved at -> {fig}')
 
 
-make_plot(a1, a2)
+give_a = lambda: rd.choice([1, 2, 3, 4, 5])
+x1 = np.arange(0, 3, 0.1)
+
+n_iterations = 5
+
+# for _ in range(n_iterations):
+#     make_plot(x1, give_a(), give_a())
+machine=platform.uname().version
+print(machine.split(' '))
