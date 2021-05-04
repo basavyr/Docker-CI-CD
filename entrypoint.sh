1e274e8e430dc9fdb774b48dc32776ef27022425
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-04 08:05:47 +0300

    improved the entrypoint script. updated env vars within the docker container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+echo 'Setting up the PATH for using pyenv and pipenv'
 export PATH="/root/.pyenv/plugins/pyenv-virtualenv/shims:/root/.pyenv/shims:/root/.pyenv/bin:$PATH"
+echo 'Setting up the LANG var'
+export LC_ALL=C
+export LANG="C.UTF-8"
+# export LC_ALL="en_US.UTF-8"
+# export LC_CTYPE="en_US.UTF-8"
 echo 'Creating the log file within the shell script'
 touch /var/log/dfcti_system_logs.log
 chmod 770 /var/log/dfcti_system_logs.log 
-echo 'Starting the log-writer procedure'
+echo 'Installing the required packages'
 cd py/log_writing
 pyenv local systems
 pipenv install
+echo 'Starting the log-writer procedure'
 pipenv run python write.py 10
 ls /var/log
