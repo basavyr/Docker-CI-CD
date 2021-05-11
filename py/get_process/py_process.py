0763e87b03dfecc8a2578fc9d43c86f2e574d410
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-11 19:43:36 +0300

    streamlined the compilation for the get_process

--- py/get_process/py_process.py
+++ py/get_process/py_process.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python
-import matplotlib.pyplot as plt
-import numpy as np
 import psutil
 
+def Check_Process_Exists(process_name):
+	#this method will look for any process with the given name within the system
+	for proc in psutil.process_iter():
+		try:
+			proc_name=str(proc.name()).lower()
+			proc_id=proc.pid
+		except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+			pass
+		if(proc_name==process_name):
+			print(f'Found 1 process with name{process_name}: {proc_name}:::{proc_id}')
+		else:
+			pass
+ 
 
 process_list = 'plist.dat'
 
 # Iterate over all running process
-with open(process_list, 'w+') as writer:
-    for proc in psutil.process_iter():
-        try:
+#with open(process_list, 'w+') as writer:
+ #   for proc in psutil.process_iter():
+  #      try:
             # Get process name & pid from process object.
-            processName = proc.name()
-            processID = proc.pid
+   #         processName = proc.name()
+    #        processID = proc.pid
             # print(processName, ' ::: ', processID)
-            writer.write(f'{str(processName).lower()} ::: {processID}\n')
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-    print('Works!')
+     #       writer.write(f'{str(processName).lower()} ::: {processID}\n')
+     #   except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+      #      pass
+   # print('Works!')
+Check_Process_Exists('python')
