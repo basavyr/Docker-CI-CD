7cebbd35f81337937607671ef9370288059bd6fe
Robert <robert.poenaru@protonmail.ch>
2021-05-12 19:30:39 +0300

    adds condition for a valid process

--- py/find_word/find_word.py
+++ py/find_word/find_word.py
@@ -4,9 +4,15 @@ import re
 
 story = 'logstash --logstash-process'
 
+proc_name='logstash'
 
-match = re.search('logstash', story.lower())
-match_2 = re.findall('logstash', story.lower())
+match = re.search('logs', story.lower())
+match_2 = re.findall('logst', story.lower())
 
 print(match.group(0))
 print(match_2)
+
+if(len(match_2)>0):
+	print(f'The process {proc_name} has been found running')
+else:
+	print(f'{proc_name} is not a running process')
