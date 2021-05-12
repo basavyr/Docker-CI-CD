f96d4eb1215a7739ef1e6e96cd40e4781e64ac3a
Robert <robert.poenaru@protonmail.ch>
2021-05-12 20:46:47 +0300

    searches for the process within a list

--- py/find_word/find_word.py
+++ py/find_word/find_word.py
@@ -12,6 +12,17 @@ match_2 = re.findall('logst', story.lower())
 print(match.group(0))
 print(match_2)
 
+process_file='plist.dat'
+
+with open(process_file,'r+') as procs:
+	processes=procs.readlines()
+	
+
+for proc in processes:
+	if(re.search('bash',proc.lower())):
+		print('found bash in proc list')
+
+
 if(len(match_2)>0):
 	print(f'The process {proc_name} has been found running')
 else:
