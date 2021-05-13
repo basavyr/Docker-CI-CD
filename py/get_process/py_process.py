89613cc444145ab03d76bb87b089a9485d7ed6f8
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-13 11:06:52 +0300

    added process class

--- py/get_process/py_process.py
+++ py/get_process/py_process.py
@@ -1,50 +1,37 @@
 #!/usr/bin/env python
 import psutil
 
-# save the processes to a file for later usage
-process_list = 'plist.dat'
 
+class Process:
+    # save the processes to a file for later usage
+    process_list = 'plist.dat'
 
-def Check_Process_Exists(process_name):
-    # this method will look for any process with the given name within the system
-    # proc_state = False
-    for proc in psutil.process_iter():
-        try:
-            proc_name = str(proc.name()).lower()
-            proc_id = proc.pid
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-        if(proc_name == process_name):
-            print(
-                f'Found 1 process with name{process_name}: {proc_name}:::{proc_id}')
-            # proc_state=True
-        else:
-            pass
-
-
-def Create_Process_List(process_list):
-    with open(process_list, 'w+') as writer:
+    @classmethod
+    def Check_Process_Exists(cls, process_list, process_name):
+        p_list = []
         for proc in psutil.process_iter():
             try:
-                processName = str(proc.name()).lower()
-                processID = proc.pid
-                writer.write(f'{processName}:::{processID}\n')
-            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                proc_name = str(proc.name()).lower()
+                proc_id = proc.pid
+                p_list.append(f'{proc_name}:::{proc_id}')
+            except(psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
                 pass
+        return p_list
+
+    @classmethod
+    def Create_Process_List(cls, process_list):
+        with open(process_list, 'w+') as writer:
+            for proc in psutil.process_iter():
+                try:
+                    processName = str(proc.name()).lower()
+                    processID = proc.pid
+                    writer.write(f'{processName}:::{processID}\n')
+                except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                    pass
+        return 1
 
 
-Create_Process_List(process_list)
-
-# Iterate over all running process
-# with open(process_list, 'w+') as writer:
-#   for proc in psutil.process_iter():
-#      try:
-# Get process name & pid from process object.
-#         processName = proc.name()
-#        processID = proc.pid
-# print(processName, ' ::: ', processID)
-#       writer.write(f'{str(processName).lower()} ::: {processID}\n')
-#   except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-#      pass
-# print('Works!')
-# Check_Process_Exists('python')
+if __name__ == '__main__':
+    proc = Process()
+    print('Running the processes app.')
+    print(f'The processes are saved into {proc.process_list}')
