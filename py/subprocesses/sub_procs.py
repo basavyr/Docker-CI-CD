13205b1c1cadcdfe863d34cddeae490d73cf2275
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-13 19:58:44 +0300

    Create sub_procs.py

---
+++ py/subprocesses/sub_procs.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from subprocess import Popen, PIPE
+import subprocess
+process = Popen(['ls', '-la'], stdout=PIPE, stderr=PIPE)
+stdout, stderr = process.communicate()
+# print(stdout)
+text=subprocess.getoutput('ls -la')
+print(text)
