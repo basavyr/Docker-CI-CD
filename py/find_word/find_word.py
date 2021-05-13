61684a45c32f4425931f270601e64286948e75f1
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-13 13:08:03 +0300

    simplified the find word procedure

--- py/find_word/find_word.py
+++ py/find_word/find_word.py
@@ -2,16 +2,6 @@
 import psutil as ps
 import re
 
-story = 'logstash --logstash-process'
-
-proc_name = 'logstash'
-
-match = re.search('logs', story.lower())
-match_2 = re.findall('logst', story.lower())
-
-print(match.group(0))
-print(match_2)
-
 process_file = 'plist.dat'
 
 with open(process_file, 'r+') as procs:
@@ -19,16 +9,6 @@ with open(process_file, 'r+') as procs:
 
 
 for proc in processes:
-    if(re.search('bash', proc.lower())):
-        print('found bash in proc list')
-
-
-if(len(match_2) > 0):
-    print(f'The process {proc_name} has been found running')
-else:
-    print(f'{proc_name} is not a running process')
-
-
-def FindProcess(process_list, process_name):
-    # searches for a process within a list
-    return 1
+    x = proc.strip()
+    if("brave" in x):
+        print(x)
