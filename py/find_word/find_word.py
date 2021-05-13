1c0d76de3f96853c0157c69310923a4444818fdd
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-13 09:38:58 +0300

    creates a list of running processes

--- py/find_word/find_word.py
+++ py/find_word/find_word.py
@@ -4,7 +4,7 @@ import re
 
 story = 'logstash --logstash-process'
 
-proc_name='logstash'
+proc_name = 'logstash'
 
 match = re.search('logs', story.lower())
 match_2 = re.findall('logst', story.lower())
@@ -12,18 +12,23 @@ match_2 = re.findall('logst', story.lower())
 print(match.group(0))
 print(match_2)
 
-process_file='plist.dat'
+process_file = 'plist.dat'
 
-with open(process_file,'r+') as procs:
-	processes=procs.readlines()
-	
+with open(process_file, 'r+') as procs:
+    processes = procs.readlines()
+
 
 for proc in processes:
-	if(re.search('bash',proc.lower())):
-		print('found bash in proc list')
+    if(re.search('bash', proc.lower())):
+        print('found bash in proc list')
 
 
-if(len(match_2)>0):
-	print(f'The process {proc_name} has been found running')
+if(len(match_2) > 0):
+    print(f'The process {proc_name} has been found running')
 else:
-	print(f'{proc_name} is not a running process')
+    print(f'{proc_name} is not a running process')
+
+
+def FindProcess(process_list, process_name):
+    # searches for a process within a list
+    return 1
--- py/get_process/py_process.py
+++ py/get_process/py_process.py
@@ -7,7 +7,7 @@ process_list = 'plist.dat'
 
 def Check_Process_Exists(process_name):
     # this method will look for any process with the given name within the system
-	# proc_state = False
+    # proc_state = False
     for proc in psutil.process_iter():
         try:
             proc_name = str(proc.name()).lower()
@@ -17,12 +17,23 @@ def Check_Process_Exists(process_name):
         if(proc_name == process_name):
             print(
                 f'Found 1 process with name{process_name}: {proc_name}:::{proc_id}')
-			# proc_state=True
+            # proc_state=True
         else:
             pass
 
 
+def Create_Process_List(process_list):
+    with open(process_list, 'w+') as writer:
+        for proc in psutil.process_iter():
+            try:
+                processName = str(proc.name()).lower()
+                processID = proc.pid
+                writer.write(f'{processName}:::{processID}\n')
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
 
+
+Create_Process_List(process_list)
 
 # Iterate over all running process
 # with open(process_list, 'w+') as writer:
