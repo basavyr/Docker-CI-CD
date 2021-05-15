63c439fb21947276df4c7f5fbb61801128c8a854
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-15 16:35:19 +0300

    add new command as output in the sub_procs

--- py/subprocesses/sub_procs.py
+++ py/subprocesses/sub_procs.py
@@ -4,5 +4,5 @@ import subprocess
 process = Popen(['ls', '-la'], stdout=PIPE, stderr=PIPE)
 stdout, stderr = process.communicate()
 # print(stdout)
-text=subprocess.getoutput('ls -la')
+text=subprocess.getoutput('echo xx')
 print(text)
