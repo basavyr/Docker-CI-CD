3bf6ffe8d9c1fdbad68b0e1a4ca6f27b9762a04a
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-17 15:27:20 +0300

    improves the output capturing

--- py/subprocesses/capture_output.py
+++ py/subprocesses/capture_output.py
@@ -4,6 +4,21 @@ import sys
 import os
 
 
+to_utf8 = lambda x: x.decode('UTF-8')
+
+
+def Get_Command_Output(command):
+    os.environ['PYTHONUNBUFFERED'] = "1"
+    proc = subprocess.Popen(command,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT,
+                            )
+    stdout, stderr = proc.communicate()
+    stdout = to_utf8(stdout)
+    stderr = to_utf8(stderr)
+    return stdout, stderr
+
+
 def run(cmd):
     os.environ['PYTHONUNBUFFERED'] = "1"
     proc = subprocess.Popen(cmd,
@@ -15,14 +30,7 @@ def run(cmd):
     return proc.returncode, stdout.decode('UTF-8'), stderr
 
 
-code, out, err = run(['ls'])
-
-
-print(f'out --> \n{out}')
-# print("err: '{}'".format(err))
-# print("exit: {}".format(code))
+print(run(['ls']))
 
 ls = subprocess.run(['ps', 'aux'], capture_output=True,
                     text=True).stdout.strip("\n")
-
-print(type(ls))
