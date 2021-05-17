e51a1f91647131e112af45f5d71ab68e20f1eb27
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-17 21:09:17 +0300

    improves piped output

--- py/subprocesses/piped_process.py
+++ py/subprocesses/piped_process.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 import subprocess
+
+
+# for shell=False use absolute paths
 p = subprocess.Popen("ps aux | grep zsh", shell=True,
                      stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-# for shell=False use absolute paths
+
+output = 'results.out'
+
 p_stdout = p.stdout.read()
 p_stderr = p.stderr.read()
-print(p_stdout.decode('UTF-8'))
+
+decoder = lambda x: x.decode('UTF-8')
+
+
+with open(output, 'w+') as save:
+    save.write(decoder(p_stdout))
