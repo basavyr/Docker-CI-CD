b5d9670368223825d9fe654b5c4e3b4f829ce3b7
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-19 15:21:53 +0300

    adds insert funciton

--- py/inserter/inserter.py
+++ py/inserter/inserter.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
 import numpy as np
 
-my_list=[1,2,3]
+my_list = [1, 2, 3]
+
+
+def insert_element(pos, element, lister):
+    try:
+        lister.insert(pos, element)
+    except Exception as exc:
+        print(
+            f'Could not insert the element {element} at position {pos} in the list: {lister}')
+    else:
+        pass
+    return
+
+
+print(my_list)
+insert_element(1, 1, my_list)
+print(my_list)
