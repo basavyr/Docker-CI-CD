a90d35c6adb41531ac667682da3da8a0308e956f
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-22 14:27:57 +0300

    runit script set to fixed time. adds bash script for starting arbitrary commands

--- py/proc_tester/multiple_runits.sh
+++ py/proc_tester/multiple_runits.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 iterations="$1"
 processName="python"
+ls_com="ls"
+runtime="nohup python runit.py &>/dev/null &" 
+direct_runtime="python runit.py &>/dev/null &"
 
 echo "Will spawn $cmd $processName instances"
 for ((c=1; c<=$iterations;c++))
 do
-echo "$c"
+nohup ./runit.py &>/dev/null &
 done
--- py/proc_tester/runit.py
+++ py/proc_tester/runit.py
@@ -3,7 +3,11 @@ import time
 
 run = True
 x = 1
+start_time = time.time()
 while(run):
     x = x+1
     print(x, x+1)
     time.sleep(1)
+    if(time.time()-start_time >= 8):
+        run = False
+        break
