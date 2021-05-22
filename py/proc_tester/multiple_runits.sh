6c4c7a1f4ce03e4a991cc881c516c02390b5fbb4
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-22 15:15:46 +0300

    cleaned the trailing characters within the script file

--- py/proc_tester/multiple_runits.sh
+++ py/proc_tester/multiple_runits.sh
@@ -5,7 +5,7 @@ processName="python"
 runtime="nohup python runit.py &>/dev/null &" 
 direct_runtime="nohup ./runit.py &>/dev/null &"
 
-echo "Will spawn $cmd $processName instances"wwww
+echo "Will spawn $cmd $processName instances"
 for ((c=1; c<=$iterations;c++))
 do
 nohup ./runit.py 10 &>/dev/null & #if the execution of the python script is called via the variables with $, the script waits for every script instance to finish
