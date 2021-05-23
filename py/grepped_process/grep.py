f85041fe6b11d1c6be987ff4af00dd00525352c1
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-23 09:37:08 +0300

    tests dmesg command in the grepped module

--- py/grepped_process/grep.py
+++ py/grepped_process/grep.py
@@ -11,13 +11,23 @@ def RunCommand(command):
     executed_command = subprocess.Popen(command, shell=True,
                                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     try:
-        output, errors = executed_command.communicate(timeout=10)
+        output, errors = executed_command.communicate(timeout=3)
     except subprocess.TimeoutExpired:
         executed_command.kill()
         output, errors = executed_command.communicate()
+    except OSError as error:
+        print(f'There was an issue with running the command.\n{error}')
+
+    # the command should stop after the try/except block
+    check_finish = executed_command.poll()
+    return_code = executed_command.returncode
+    print(f'Finished status: {check_finish}\nReturn code: {return_code}')
+
     return output, errors
 
 
-command = "ls -la"
+listed_command = ["dmesg"]
+string_command = "ls -la"
 
-print(RunCommand(command)[0].decode(utf8))
+print(RunCommand(listed_command))
+# print(RunCommand(string_command)[0].decode(utf8))
