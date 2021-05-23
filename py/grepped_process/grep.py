8f3a690fd3cbef2686f1f383e43fffd39b20111d
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-23 14:40:56 +0300

    checks if the command ran properly or not

--- py/grepped_process/grep.py
+++ py/grepped_process/grep.py
@@ -19,15 +19,23 @@ def RunCommand(command):
         print(f'There was an issue with running the command.\n{error}')
 
     # the command should stop after the try/except block
-    check_finish = executed_command.poll()
-    return_code = executed_command.returncode
-    print(f'Finished status: {check_finish}\nReturn code: {return_code}')
+    # check_finish = executed_command.poll()
+    # return_code = executed_command.returncode
+    # print(f'Finished status: {check_finish}\nReturn code: {return_code}')
+    Check_Command_Status(executed_command)
 
     return output, errors
+
+
+def Check_Command_Status(command):
+    if(command.returncode == 0):
+        print(f'The command has been executed successfully')
+    else:
+        print('There was an issue running the command')
 
 
 listed_command = "dmesg --help"
 string_command = "ls -la"
 
-print(RunCommand(listed_command))
+RunCommand(listed_command)
 # print(RunCommand(string_command)[0].decode(utf8))
