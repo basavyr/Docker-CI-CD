64bb6f96d54f576585be06fb75d1f6682db77670
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-05-31 20:10:59 +0300

    Update test_module1.py

--- py/import_packages_test/tests/test_module1.py
+++ py/import_packages_test/tests/test_module1.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python
 
 import sys
+
+try:
+    from module1 import m1_scrip1
+    from module1 import m1_scrip2
+except ImportError:
+    sys.path.append('../')
+    from module1 import m1_scrip1
+    from module1 import m1_scrip2
+
+
+m1_scrip2.Main()
+print(m1_scrip1.Script1.ARGX)
