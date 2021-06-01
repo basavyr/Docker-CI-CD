f2c1490e1ea0baccf9eda01e2da42eb84c7f83ed
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-01 11:31:51 +0300

    fixes execution of the test within the cwd and a higher directory level

--- py/import_packages_test/tests/test_module1.py
+++ py/import_packages_test/tests/test_module1.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
 import sys
-
+import os
 try:
-    sys.path.append('module1/')
-    from .module import m1_scrip1
-    from .module import m1_scrip2
-except ImportError:
     sys.path.append('../')
     from module1 import m1_scrip1
     from module1 import m1_scrip2
-
+except ImportError:
+    print('failed')
+    sys.path.append('.')
+    from module1 import m1_scrip1
+    from module1 import m1_scrip2
 
 m1_scrip2.Main()
 print(m1_scrip1.Script1.ARGX)
