90480227ca31f31ef2ab6a5993618696f73b92cf
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-01 11:55:05 +0300

    Update run_scripts.sh

--- py/import_packages_test/run_scripts.sh
+++ py/import_packages_test/run_scripts.sh
@@ -5,7 +5,7 @@ sleep 1
 python test_scripts.py
 echo 
 echo 'Running the test components for <<module1>>...'
-sleep 1
-cd tests
-echo 'cd-ing into tests/ -> '
-python test_module1.py
+# sleep 1
+# cd tests
+# echo 'cd-ing into tests/ -> '
+python tests/test_module1.py
