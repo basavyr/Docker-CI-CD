cba304749f8a775ae3120672863498dc181edd99
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-08-10 20:10:44 +0300

    add random data generator

---
+++ py/average-values/avgs.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import random as rd
+
+
+randomData=rd.choice([1,2,3],size=100)
+
+print(randomData)
---
+++ py/average-values/description.md
@@ -0,0 +1,3 @@
+# Average values
+
+Calculates the average values of some measurements done for a quantity.
