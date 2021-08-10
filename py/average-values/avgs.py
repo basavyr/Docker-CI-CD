fb77b9e2314fb9a95820631b75f2f20145ecfd43
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-08-10 20:20:14 +0300

    Update avgs.py

--- py/average-values/avgs.py
+++ py/average-values/avgs.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 from numpy import random as rd
 
 
-randomData=rd.choice([1,2,3],size=100)
+randomData = rd.choice([1, 2, 3], size=100)
+
+fct = lambda x: 2 * x + 1
+
+mathValues = list(map(fct,randomData))
 
 print(randomData)
+
+print(mathValues)
